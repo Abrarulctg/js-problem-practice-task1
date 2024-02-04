43b7155f940ece9de80c5ba1aa1c4badd80549c8
Abrarulctg <jubairctg99@gmail.com>
2024-02-05 00:32:00 +0600

    finished task 2

---
+++ task-2.js
@@ -0,0 +1,16 @@
+// You are given an array of numbers. Count how many times the a number is repeated in the array.
+
+function repeatedNum(numbers, rep) {
+    let find = 0;
+    for (const number of numbers) {
+        // console.log(number)
+        if (number === rep) {
+            find = find + 1;
+        }
+    }
+    console.log(find);
+}
+
+const numbers = [5, 6, 11, 12, 98, 5, 56, 11, 5];
+repeatedNum(numbers, 5);
+// console.log();
